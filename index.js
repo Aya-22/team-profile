5df53116f22402a2d8a95506b2fcf5493a6d73dd
Jennifer Ayala <j.ayala0695@gmail.com>
2021-08-25 21:48:47 -0700

    pseudo code added to index.js, no changes to employeeClass.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // inquirer multiple lead manager to employee types
+// function for create manager role
+// function to ask if additional employees need to be added
+// have a choice option to select from engineer, intern
+// function to create engineer role
+// call back employee type question after
+// function ro create intern role
+// function to generate html file
+// const for html file format
